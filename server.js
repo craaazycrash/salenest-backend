083a586a3b10f46c9dfd382a770f1542bec29a89
craaazycrash <craaazycrash@gmail.com>
2026-04-11 13:31:48 +0530

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://salenest-frontend.vercel.app/"],
+  origin: ["http://localhost:5173", "https://salenest-frontend.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
